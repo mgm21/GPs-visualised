37facc24f259a33d7360fa214f3869bdda697500
mgm21 <mgm18@ic.ac.uk>
2023-07-01 16:03:45 +0100

    Optimised some methods in GaussianProcess class

--- source/gaussian_process.py
+++ source/gaussian_process.py
@@ -9,12 +9,12 @@ class GaussianProcess:
         self.length_scale = length_scale
         self.kappa = kappa
 
-        # Problem parameters (akin to quality-diversity behavioural descriptor bounds and resolution)
+        # Problem parameters
         self.x_start, self.x_stop = 0, 10
         self.num_points = 100
         self.x_problem = np.linspace(start=self.x_start, stop=self.x_stop, num=self.num_points)
-        self.x_seen = []
-        self.y_seen = []
+        self.x_seen = np.array([])
+        self.y_seen = np.array([])
 
         # Plotting parameters
         self.plot_col = "cornflowerblue"
@@ -22,66 +22,59 @@ class GaussianProcess:
 
     def kernel_func(self, x1, x2):
         # Squared exponential kernel (naive but works for 1-D toy example) see Brochu tutorial p.9
-        return np.exp(-0.5 * (np.abs(x2 - x1) / self.length_scale) ** 2)
+        return np.exp(-0.5 * ((x2 - x1) / self.length_scale) ** 2)
 
     def mu_0(self, x):
-        return np.zeros(shape=len(x))
+        return np.sin(3 * x) + 1.5 * x
 
-    def var_0(self, x):
-        # Never called.
-        return np.array([self.kernel_func(xi, xi) for xi in x]) + self.sampling_noise
+    # def var_0(self, x):
+    #     # Never called
+    #     return np.array([self.kernel_func(xi, xi) for xi in x]) + self.sampling_noise
 
-    # TODO: think of a way to have a different true_func for every GP, though maybe the same prior...
-    #  maybe best is to define the true function as both a list (attribute) and a function which produced it
-    #  and then define functions to move back and forth between the index world and the x value world.
-    #  BOOKMARK
     def true_func(self, x):
-        return [sum(x) for x in zip([np.sin(3*j) for j in x], [2 * i for i in x])]
+        return np.sin(3 * x) + 2 * x
 
-    # TODO: replace all the x_seen, y_seen, etc... occurrences with the attributes from the class self.x_seen,
-    #  etc... (maybe?): think about how it will work when you have several
-    # TODO: best thing for now is to keep because it does not hurt anything else than readability; meaning, develop
-    #  the code further to know if it was required or not and then can remove once an MVP of the complete code is done.
     def k_vec(self, x, x_seen):
         return [self.kernel_func(x, xi) for xi in x_seen]
 
     def K_mat(self, x_seen):
-        mat = np.zeros(shape=(len(x_seen), len(x_seen)))
-        for i in range(len(x_seen)):
-            for j in range(len(x_seen)):
+        n = len(x_seen)
+        mat = np.zeros(shape=(n, n))
+        for i in range(n):
+            for j in range(i, n):
+                # Using the symmetry of the kernel matrix
                 mat[i, j] = self.kernel_func(x_seen[i], x_seen[j])
         return mat
 
     def mu_new(self, x, x_seen, y_seen):
-        K_computed = self.K_mat(x_seen)
-        k_computed = self.k_vec(x, x_seen)
-        return self.mu_0(x) + np.transpose(k_computed) @ np.linalg.inv(
-            K_computed + self.sampling_noise * np.identity(n=np.shape(K_computed)[0])) @ (y_seen - self.mu_0(x_seen))
+        K = self.K_mat(x_seen)
+        k = self.k_vec(x, x_seen)
+        mu_new = self.mu_0(x) + np.transpose(k) @ np.linalg.inv(
+            K + self.sampling_noise * np.identity(n=np.shape(K)[0])) @ (y_seen - self.mu_0(x_seen))
+        return mu_new
 
     def var_new(self, x, x_seen):
         K_computed = self.K_mat(x_seen)
-
         var = []
         for i in range(len(x)):
             var += [self.kernel_func(x[i], x[i]) + self.sampling_noise - np.transpose(
                 self.k_vec(x[i], x_seen)) @ np.linalg.inv(
                 K_computed + self.sampling_noise * np.identity(n=np.shape(K_computed)[0])) @ self.k_vec(x[i], x_seen)]
-
         return var
 
     def observe_true_points(self, x):
-        self.x_seen.extend(x)
-        self.y_seen.extend(self.true_func(x))
+        x = np.array(x)
+        self.x_seen = np.append(self.x_seen, x)
+        self.y_seen = np.append(self.y_seen, self.true_func(x))
 
     def query_acquisition_function(self):
-
         gp_mean = self.mu_new(self.x_problem, self.x_seen, self.y_seen)
         gp_var = self.var_new(self.x_problem, self.x_seen)
 
         max_val_index = np.argmax(gp_mean + [i * self.kappa for i in gp_var])
-        max_val_xloc = (self.x_stop - self.x_start) * (max_val_index + 1) / self.num_points
+        max_val_x_loc = (self.x_stop - self.x_start) * (max_val_index + 1) / self.num_points
 
-        return max_val_xloc
+        return max_val_x_loc
 
     def plot_all(self, savefig=True):
         # Set up the plotting environment
@@ -115,3 +108,15 @@ class GaussianProcess:
             plt.savefig("example-sin", dpi=20)
 
         plt.show()
+
+# TODO:
+
+# TODO: replace all the x_seen, y_seen, etc... occurrences with the attributes from the class self.x_seen,
+#  etc... (maybe?): think about how it will work when you have several
+
+# TODO: best thing for now is to keep because it does not hurt anything else than readability; meaning, develop
+#  the code further to know if it was required or not and then can remove once an MVP of the complete code is done.
+
+# TODO: think of a way to have a different true_func for every GP, though maybe the same prior...
+#  maybe best is to define the true function as both a list (attribute) and a function which produced it
+#  and then define functions to move back and forth between the index world and the x value world.
--- source/main.py
+++ source/main.py
@@ -6,7 +6,7 @@ agent = GaussianProcess(sampling_noise=0, kappa=5)
 
 # TODO: try to understand why the trialled optimisation using ITE below with the acquisition did not converge
 # Make agent observe some true points
-agent.observe_true_points([])
+agent.observe_true_points([5, 7])
 print(agent.query_acquisition_function())
 
 # Plot
