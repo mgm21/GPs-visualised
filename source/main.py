a8e9d8c518f1087a92b71a15c757aa887fbc2ac7
mgm21 <mgm18@ic.ac.uk>
2023-07-02 22:23:12 +0100

    Added some useful todo comments in main.py

--- source/main.py
+++ source/main.py
@@ -43,5 +43,22 @@ visualiser = Visualiser()
 # visualiser.visualise_ITE_experiment(agents[5], alpha=0.9)
 # visualiser.visualise_example_experiment(agents[:])
 
+# TODO: in any collaborative setting with multiple curves being plotted, you need to pass in as options which curves
+#  are being plotted. For example, it is unnecssary in the collaborative setting to plot the true function, or the
+#  the seen points (this can be seen easily). Maybe you can make the default for some curves not to be seen but you can
+#  toggle the option if you so please.
+
+# TODO: think about whether you want to decouple the application parts (all the launch app) from the plotting. I think
+#  that they are intriniscally linked because the application is a plottling app and therefore do not believe that they
+#  they need different classes.
+
+# TODO: clean up the whole code, and think about whether a better way of displaying the knowledge as the way it is being
+#  displayed now. Maybe without all the uncertainties for all the parents... Maybe make that initially hidden and that it
+#  can be toggled. That way, you can have all the information at hand, but it is easy to see in the default setting.
+
+# TODO: strech goal but not necessary, make the plotting better whereby you do not re-plot the entire thing each time
+#  but only modify certain objects, if you could somehow identify which objects you had to change etc... this is a strech
+#  goal as it does not impact the overall project (this is just a toy problem meant for visualisation)
+
 # TODO: try to understand why increasing the sampling noise makes the mean no longer go through the true function
 #  curve (is this normal?)
